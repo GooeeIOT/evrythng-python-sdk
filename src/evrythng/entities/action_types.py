6e82bbec1271e4862f843ecfac5ac3492e625a0f
Lyle Scott, III <lyle@digitalfoo.net>
2015-12-11 11:21:41 -0500

    Fixup action_type API since evrythng fixed their documentation bug

--- src/evrythng/entities/action_types.py
+++ src/evrythng/entities/action_types.py
@@ -1,23 +1,16 @@
 from evrythng import assertions, utils
 from evrythng.exceptions import RequiredFieldException
 
-'''
+
 datatype_specs = {
-    'type': 'str',
-    'thng': 'ref',
-    'product': 'ref',
-    'collection': 'ref',
-    'timestamp': 'time',
-    'identifiers': 'dict_of_dict',
-    'location': 'location',
-    'locationSource': 'str',
-    'context': 'dict',
+    'name': 'str',
     'customFields': 'dict_of_str',
+    'tags': 'dict_of_str',
+    'scopes': 'scopes',
 }
-required_fields = ('type',)
-readonly_fields = ('id', 'user', 'createdAt', 'createdByProject, createdByApp')
-writable_fields = ('thng', 'product', 'collection', 'timestamp', 'identifiers',
-                   'location', 'locationSource', 'context', 'customFields')
+required_fields = ('name',)
+readonly_fields = ('id', 'createdAt', 'updatedAt')
+writable_fields = ('customFields', 'tags', 'scopes')
 
 
 def _validate_data(kwargs):
@@ -34,9 +27,7 @@ def _validate_data(kwargs):
     assertions.datatypes(kwargs, datatype_specs)
 
 
-def create_action_type(type_, thng=None, product=None, collection=None,
-                       timestamp=None, identifiers=None, location=None,
-                       locationSource=None, context=None, customFields=None,
+def create_action_type(name, customFields=None, tags=None, scopes=None,
                        api_key=None):
     kwargs = locals()
     kwargs['type'] = kwargs['type_']
@@ -68,4 +59,3 @@ def list_action_types(type_, api_key=None):
 def read_action_type(type_, action_id, api_key=None):
     url = '/actions/{}/{}'.format(type_, action_id)
     return utils.request('GET', url, api_key=api_key)
-'''
