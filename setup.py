38abc2d155492e1dadb507a63a484f2147118163
Lyle Scott, III <lyle@digitalfoo.net>
2015-12-14 08:48:08 -0500

    Create a pip installable project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='python-evrythng',
+    version='0.1',
+    packages=['evrythng', 'evrythng.entities'],
+    package_dir={'': 'src'},
+    url='https://github.com/GooeeIOT/python-evrythng',
+    license='MIT',
+    author='Lyle Scott, III',
+    author_email='lyle@digitalfoo.net',
+    description='A Python wrapper about the Evrythng REST API.'
+)
