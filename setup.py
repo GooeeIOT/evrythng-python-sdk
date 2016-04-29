6962905dfa0a12c181346c353db1ffeb1108c8a5
Dairon Medina <dairon.medina@gmail.com>
2016-04-29 02:05:50 -0400

    Fixed package setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Utilities',
         'Topic :: Software Development :: Libraries'
     ],
