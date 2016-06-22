5375ddcf984a5d5dab86351c4df30b6a43a8f857
Ramon Alvarez <ramon@gooee.com>
2016-06-22 16:28:05 -0400

    Added login_user and missing doc strings to application_users.

--- src/evrythng/entities/application_users.py
+++ src/evrythng/entities/application_users.py
@@ -29,6 +29,7 @@ field_specs = {
 def create_user(email, firstName=None, lastName=None, password=None,
                 birthday=None, gender=None, timezone=None, locale=None,
                 photo=None, customFields=None, tags=None, api_key=None):
+    """Create an Application User."""
     kwargs = locals()
     api_key = kwargs.pop('api_key', None)
     assertions.validate_field_specs(kwargs, field_specs)
@@ -37,6 +38,7 @@ def create_user(email, firstName=None, lastName=None, password=None,
 
 
 def activate_user(user_id, activationCode, api_key=None):
+    """Activate an Application User."""
     assertions.datatype_str('user_id', user_id)
     assertions.datatype_str('activationCode', activationCode)
     url = '/auth/evrythng/users/{}/validate'.format(user_id)
@@ -45,6 +47,7 @@ def activate_user(user_id, activationCode, api_key=None):
 
 
 def create_anonymous_user(api_key=None):
+    """Create an Anonymous Application User."""
     return utils.request(
         'POST', '/auth/evrythng/users?anonymous=true', api_key=api_key)
 
@@ -63,6 +66,7 @@ def authenticate_user(email, password, api_key=None):
 
 
 def authenticate_facebook_user(expires, token, api_key=None):
+    """Authenticate a Facebook User."""
     assertions.datatype_str('token', token)
     data = {
         'access': {
@@ -74,22 +78,33 @@ def authenticate_facebook_user(expires, token, api_key=None):
 
 
 def delete_user(user_id, api_key=None):
-    """Delete a User"""
+    """Delete a User."""
     assertions.datatype_str('user_id', user_id)
     url = '/users/{}'.format(user_id)
     return utils.request('DELETE', url, api_key=api_key)
 
 
 def logout_user(api_key=None):
+    """Logout an Application User."""
     return utils.request('POST', '/auth/all/logout', api_key=api_key)
 
 
+def login_user(email, password, app_api_key):
+    """Login an Application User."""
+    kwargs = locals()
+    app_api_key = kwargs.pop('app_api_key', None)
+    assertions.validate_field_specs(kwargs, field_specs)
+    return utils.request('POST', '/auth/evrythng', data=kwargs, api_key=app_api_key)
+
+
 def list_users(project_id, api_key=None):
+    """List Application Users."""
     assertions.datatype_str('project_id', project_id)
     return utils.request('GET', '/users', api_key=api_key)
 
 
 def read_user(user_id, api_key=None):
+    """Read an Application User."""
     assertions.datatype_str('user_id', user_id)
     url = '/users/{}'.format(user_id)
     return utils.request('GET', url, api_key=api_key)
@@ -99,6 +114,7 @@ def update_user(user_id, email=None, firstName=None, lastName=None,
                 password=None, birthday=None, gender=None, timezone=None,
                 locale=None, photo=None, customFields=None, tags=None,
                 api_key=None):
+    """Update an Application User."""
     kwargs = locals()
     user_id = kwargs.pop('user_id')
     api_key = kwargs.pop('api_key', None)
