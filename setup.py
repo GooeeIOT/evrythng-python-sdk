4757ae97fdf6779a29be1a569c2b61bd6cdf755b
Dustin Rohde <dustin.rohde@gmail.com>
2017-11-08 12:01:36 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='python-evrythng',
-    version='0.2.21',
+    version='0.2.22',
     packages=['evrythng', 'evrythng.entities', 'evrythng.extended'],
     package_dir={'': 'src'},
     include_package_data=True,
