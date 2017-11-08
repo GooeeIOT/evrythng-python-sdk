96d224a5106197ad600f9d4b50de79f99c3f8843
Dustin Rohde <dustin.rohde@gmail.com>
2017-11-08 13:43:01 -0500

    Version 0.2.23

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='python-evrythng',
-    version='0.2.22',
+    version='0.2.23',
     packages=['evrythng', 'evrythng.entities', 'evrythng.extended'],
     package_dir={'': 'src'},
     include_package_data=True,
